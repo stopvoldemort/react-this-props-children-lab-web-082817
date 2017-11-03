c806127468566cb26627f06a2d249cce35c8cf8a
stopvoldemort <david.squires1@gmail.com>
2017-11-03 13:44:02 -0400

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,19 @@
 // Code Invitation Component Here
+import React, { Component } from 'react'
+
+class Invitation extends Component {
+  render() {
+    const childrenDivs = React.Children.map(this.props.children, child => {
+      return <div>{child}</div>
+    })
+
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {childrenDivs}
+      </div>
+    )
+  }
+}
+
+export default Invitation
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,20 @@
 // Code ThemedDecoration Component Here
+import React, { Component } from 'react'
+
+class ThemedDecoration extends Component {
+  render() {
+    const DecorationsWithClasses = React.Children.map(this.props.children, child => {
+      return (
+        React.cloneElement(child, {className: this.props.theme})
+      )
+    })
+
+    return (
+      <div>
+        {DecorationsWithClasses}
+      </div>
+    )
+  }
+}
+
+export default ThemedDecoration
